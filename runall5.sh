7db92289c813d7d335e13853e90be9bc9ca7c2dd
fsgeek <fsgeek@gatech.edu>
2019-06-20 21:45:39 -0400

    Update with additional results

--- runall5.sh
+++ runall5.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-./runall.sh
-./runall.sh
-./runall.sh
-./runall.sh
-./runall.sh
+timestamp=`date '+%Y_%m_%d__%H_%M_%S'`;
+
+./runall.sh >& runall-$timestamp-run-1.log
+./runall.sh >& runall-$timestamp-run-2.log
+./runall.sh >& runall-$timestamp-run-3.log
+./runall.sh >& runall-$timestamp-run-4.log
+./runall.sh >& runall-$timestamp-run-5.log
 
