404a6059f083e8c972ca37c2fb29a2914419420b
fsgeek <fsgeek@gatech.edu>
2019-07-20 10:40:01 -0400

    Don't run perf twice

--- wrapper-comerge.sh
+++ wrapper-comerge.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo $4
-echo VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=libvmmalloc.so.1 perf stat -B -d -d -d $3
-VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=libvmmalloc.so.1 perf stat -B -d -d -d $3 >> $4 2>&1
+echo VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=libvmmalloc.so.1 $3 >> $4
+VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=libvmmalloc.so.1 $3 >> $4 2>&1
 
