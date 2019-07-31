75dbabd1000cfbaeca447747e84b9cfc2e5294dd
fsgeek <fsgeek@gatech.edu>
2019-07-31 08:49:19 -0400

    Add scripts used.  Add data from run with numactl

--- run-comerge-aep.sh
+++ run-comerge-aep.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-VMMALLOC_POOL_SIZE=$((5*1024*1024*1024))
+VMMALLOC_POOL_SIZE=$((32*1024*1024*1024))
 #export VMMALLOC_POOL_DIR="/mnt/pmem7/fsgeek"
 
 #LD_PRELOAD=libvmmalloc.so.1 grep "pmem" /proc/mounts
@@ -62,9 +62,9 @@ make -f Makefile.comerge >> $MAKE_LOG 2>&1
 for b in $BENCH
 do
     echo "Start $b"
-    echo $b >> $PMEM7_FILE
-    aeplog="$RESULTS_DIR"/aep-pmem7-"$(basename -- $b)"-"$timestamp".csv"
-    echo "AEPWatch 1 -f "$aeplog >> $PMEM7_FILE 2>&1
+    echo hwloc-bind --single node:$node_pmem7 --verbose -- perf stat -B -d -d -d ./wrapper-comerge.sh $PMEM7_POOL_DIR $VMMALLOC_POOL_SIZE $b $PMEM7_FILE >> $PMEM7_FILE 2>&1
+    aeplog=$RESULTS_DIR"/aep-pmem7-"$(basename -- $b)"-"$timestamp".csv"
+    echo "AEPWatch 1 -f "$aeplog >> $PMEM7_FILE
     AEPWatch 1 -f $aeplog &
     hwloc-bind --single node:$node_pmem7 --verbose -- perf stat -B -d -d -d ./wrapper-comerge.sh $PMEM7_POOL_DIR $VMMALLOC_POOL_SIZE $b $PMEM7_FILE >> $PMEM7_FILE 2>&1
     sleep 128
@@ -72,20 +72,21 @@ do
     AEPWatch-stop >> $PMEM7_FILE 2>&1
     echo finished >> $PMEM7_FILE
 
-    echo $b >> $PMEM1_FILE
-    aeplog="$RESULTS_DIR"/aep-pmem1-"$(basename -- $b)"-"$timestamp".csv"
-    echo "AEPWatch 1 -f "$aeplog >> $PMEM1_FILE 2>&1
+    echo hwloc-bind --single node:$node_pmem1 --verbose -- perf stat -B -d -d -d ./wrapper-comerge.sh $PMEM1_POOL_DIR $VMMALLOC_POOL_SIZE $b $PMEM1_FILE >> $PMEM1_FILE
+    aeplog=$RESULTS_DIR"/aep-pmem1-"$(basename -- $b)"-"$timestamp".csv"
+    echo "AEPWatch 1 -f "$aeplog >> $PMEM1_FILE
     AEPWatch 1 -f $aeplog &
     hwloc-bind --single node:$node_pmem1 --verbose -- perf stat -B -d -d -d ./wrapper-comerge.sh $PMEM1_POOL_DIR $VMMALLOC_POOL_SIZE $b $PMEM1_FILE >> $PMEM1_FILE 2>&1
     sleep 128
-    echo "AEPWatch-stop" >>$PMEM1_FILE 2>&1
+    echo "AEPWatch-stop" >>$PMEM1_FILE
     AEPWatch-stop >> $PMEM1_FILE 2>&1
     echo finished >> $PMEM1_FILE
     
-    echo $b >> $DRAM0_FILE
+    echo hwloc-bind --single node:0  --verbose -- perf stat -B -d -d -d $b >>$DRAM0_FILE 2>&1
     hwloc-bind --single node:0  --verbose -- perf stat -B -d -d -d $b >>$DRAM0_FILE 2>&1
     echo finished >> $DRAM0_FILE
-    echo $b >> $DRAM1_FILE
+
+    echo hwloc-bind --single node:1 --verbose -- perf stat -B -d -d -d $b >>$DRAM1_FILE 2>&1
     hwloc-bind --single node:1 --verbose -- perf stat -B -d -d -d $b >>$DRAM1_FILE 2>&1
     echo finished >> $DRAM1_FILE
     echo "End $b"
--- run-comerge1.sh
+++ run-comerge1.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#waittime=18000 # 60 * 60 * 5 = 5 hours
+#echo sleep $waittime
+#sleep $waittime
+
 timestamp=`date '+%Y_%m_%d__%H_%M_%S'`;
 
 ./run-comerge-aep.sh >& run-comerge-$timestamp-run-1.log
