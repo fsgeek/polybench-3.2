5a9d7422eb58996a6912b7d9c73a639ed364beee
fsgeek <fsgeek@gatech.edu>
2019-08-09 08:24:15 -0400

    Add test results using pmem7 (striped)

---
+++ run-comerge-src-pmem7.sh
@@ -0,0 +1,94 @@
+#/bin/sh
+
+VMMALLOC_POOL_SIZE=$((5*1024*1024*1024))
+
+#LD_PRELOAD=libvmmalloc.so.1 grep "pmem" /proc/mounts
+node_pmem4=0
+node_pmem3=0
+node_pmem7=0
+timestamp=`date '+%Y_%m_%d__%H_%M_%S'`;
+hostname=`hostname`
+
+# log files
+RESULTS_DIR="./comerge-pb-src-pmem7-results-"$timestamp
+MAKE_LOG=$RESULTS_DIR"/pb-make-"$hostname"-"$timestamp".log"
+PMEM7_FILE=$RESULTS_DIR"/pb-pmem7-"$hostname"-"$timestamp".log"
+PMEM7H_FILE=$RESULTS_DIR"/pb-pmem7h-"$hostname"-"$timestamp".log"
+PMEM7_POOL_DIR="/mnt/pmem7/fsgeek"
+
+BENCH="
+./linear-algebra/kernels/2mm/2mm_time
+./linear-algebra/kernels/3mm/3mm_time
+./linear-algebra/kernels/atax/atax_time
+./linear-algebra/kernels/bicg/bicg_time
+./linear-algebra/kernels/cholesky/cholesky_time
+./linear-algebra/kernels/doitgen/doitgen_time
+./linear-algebra/kernels/gemm/gemm_time
+./linear-algebra/kernels/gemver/gemver_time
+./linear-algebra/kernels/gesummv/gesummv_time
+./linear-algebra/kernels/mvt/mvt_time 
+./linear-algebra/kernels/symm/symm_time
+./linear-algebra/kernels/syr2k/syr2k_time
+./linear-algebra/kernels/syrk/syrk_time
+./linear-algebra/kernels/trisolv/trisolv_time
+./linear-algebra/kernels/trmm/trmm_time
+./linear-algebra/solvers/durbin/durbin_time
+./linear-algebra/solvers/dynprog/dynprog_time
+./linear-algebra/solvers/gramschmidt/gramschmidt_time
+./linear-algebra/solvers/lu/lu_time
+./linear-algebra/solvers/ludcmp/ludcmp_time
+./datamining/correlation/correlation_time
+./datamining/covariance/covariance_time
+./medley/floyd-warshall/floyd-warshall_time
+./medley/reg_detect/reg_detect_time
+./stencils/adi/adi_time
+./stencils/fdtd-2d/fdtd-2d_time
+./stencils/fdtd-apml/fdtd-apml_time
+./stencils/jacobi-1d-imper/jacobi-1d-imper_time
+./stencils/jacobi-2d-imper/jacobi-2d-imper_time
+./stencils/seidel-2d/seidel-2d_time
+"
+
+[ ! -d $RESULTS_DIR ] && mkdir $RESULTS_DIR && echo "mkdir $RESULTS_DIR"
+
+## Rebuild
+make -f Makefile.comerge >> $MAKE_LOG 2>&1
+
+# Partition the disk
+umount /mnt/pmem7
+parted -s -a optimal -- /dev/pmem7 mklabel gpt mkpart primary ext4 1MiB -4096s >> $PMEM7_FILE 2>&1
+mkfs.ext4 -b 4096 -F /dev/pmem7p1 >> $PMEM7_FILE 2>&1
+fdisk -l /dev/pmem7 >> $PMEM7_FILE 2>&1
+mount -o dax /dev/pmem7p1 /mnt/pmem7
+
+[ ! -d $PMEM7_POOL_DIR ] && mkdir $PMEM7_POOL_DIR && echo "mkdir $PMEM7_POOL_DIR"
+
+## Run all benchmarks
+for b in $BENCH
+do
+    echo "Start $b"
+    echo $b >> $PMEM7_FILE
+    hwloc-bind --single node:$node_pmem7 --verbose -- perf stat -B -d -d -d ./wrapper-comerge.sh $PMEM7_POOL_DIR $VMMALLOC_POOL_SIZE $b $PMEM7_FILE >> $PMEM7_FILE 2>&1
+    echo finished >> $PMEM7_FILE
+    echo "End $b"
+done
+
+umount /mnt/pmem7
+parted -s -a optimal -- /dev/pmem7 mklabel gpt mkpart primary 2MiB -4096s >> $PMEM7H_FILE 2>&1
+mkfs.ext4 -b 4096 -E stride=512 -F /dev/pmem7p1 >> $PMEM7H_FILE 2>&1
+fdisk -l /dev/pmem7 >> $PMEM7H_FILE 2>&1
+mount -o dax /dev/pmem7p1 /mnt/pmem7
+
+[ ! -d $PMEM7_POOL_DIR ] && mkdir $PMEM7_POOL_DIR && echo "mkdir $PMEM7_POOL_DIR"
+
+## Run all benchmarks
+for b in $BENCH
+do
+    echo "Start $b"
+    echo $b >> $PMEM7H_FILE
+    hwloc-bind --single node:$node_pmem7 --verbose -- perf stat -B -d -d -d ./wrapper-comerge.sh $PMEM7_POOL_DIR $VMMALLOC_POOL_SIZE $b $PMEM7H_FILE >> $PMEM7H_FILE 2>&1
+    echo finished >> $PMEM7H_FILE
+    echo "End $b"
+done
+
+umount /mnt/pmem7
